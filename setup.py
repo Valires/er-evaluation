fafbff63a5446cb97bbac558d4cd0922467a0e1e
OlivierBinette <olivier.binette@gmail.com>
2023-01-11 22:11:02 -0500

    update version

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/OlivierBinette/er_evaluation",
-    version="2.0.0",
+    version="1.2.0",
     zip_safe=False,
 )
