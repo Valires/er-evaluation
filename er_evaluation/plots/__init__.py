cc53352832f9b8c4316b1f6e19c6c3e523955749
OlivierBinette <olivier.binette@gmail.com>
2023-04-06 17:59:16 +0000

    Add regression tree pipeline and sunburst visualization

--- er_evaluation/plots/__init__.py
+++ er_evaluation/plots/__init__.py
@@ -13,6 +13,7 @@ from er_evaluation.plots._plots import (
     plot_metrics,
     plot_summaries,
 )
+from er_evaluation.plots._sunburst import plot_dt_regressor_sunburst
 
 __all__ = [
     "add_ests_to_summaries",
@@ -20,6 +21,7 @@ __all__ = [
     "plot_cluster_errors",
     "plot_cluster_sizes_distribution",
     "plot_comparison",
+    "plot_dt_regressor_sunburst",
     "plot_entropy_curve",
     "plot_estimates",
     "plot_metrics",
